8e19976759002794c57a6e9c8b7183ea2344ef42
mirametodieva <mirushankarr@gmail.com>
2025-04-25 17:39:42 +0300

    Init the project

---
+++ src/main/java/com/getthecolor/nailtonebe/NailToneBeApplication.java
@@ -0,0 +1,13 @@
+package com.getthecolor.nailtonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailToneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailToneBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getthecolor/nailtonebe/NailToneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getthecolor.nailtonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailToneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
