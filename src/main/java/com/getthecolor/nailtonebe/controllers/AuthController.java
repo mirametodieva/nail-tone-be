76fa6672ff3ada1ad6615cc77ec12030cd237982
mirametodieva <mirushankarr@gmail.com>
2025-05-06 16:18:03 +0300

    Update auth responses

--- src/main/java/com/getthecolor/nailtonebe/controllers/AuthController.java
+++ src/main/java/com/getthecolor/nailtonebe/controllers/AuthController.java
@@ -4,10 +4,10 @@ import com.getthecolor.nailtonebe.controllers.models.LoginModel;
 import com.getthecolor.nailtonebe.controllers.models.RegistrationModel;
 import com.getthecolor.nailtonebe.services.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,14 +20,14 @@ public class AuthController {
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<String> register(@RequestBody RegistrationModel model) {
+    public ResponseEntity<Map<String, String>> register(@RequestBody RegistrationModel model) {
         String token = authService.register(model);
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(Collections.singletonMap("token", token));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginModel model) {
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginModel model) {
         String token = authService.login(model);
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(Collections.singletonMap("token", token));
     }
 }
