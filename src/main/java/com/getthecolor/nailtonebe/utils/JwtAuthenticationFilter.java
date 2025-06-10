bb18bc0337301e6aee2dc7fa5b18259e0a1a0ce2
S_Stoykov26 <thelifeishere@abv.bg>
2025-06-10 23:02:18 +0300

    debug cors

--- src/main/java/com/getthecolor/nailtonebe/utils/JwtAuthenticationFilter.java
+++ src/main/java/com/getthecolor/nailtonebe/utils/JwtAuthenticationFilter.java
@@ -25,6 +25,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+
+        if (path.startsWith("/api/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         String authHeader = request.getHeader("Authorization");
 
